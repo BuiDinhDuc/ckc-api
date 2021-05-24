806ede463b03a3f4462bce80a115e4aba413fe6a
BuiDinhDuc <dinhduc2121997@gmail.com>
2021-05-24 21:04:28 +0700

    api sinh vien

--- app/Http/Controllers/SinhVienController.php
+++ app/Http/Controllers/SinhVienController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\SinhVien;
-
+use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class SinhVienController extends Controller
 {
@@ -14,7 +15,7 @@ class SinhVienController extends Controller
     // }
     public function getAllSinhVien(Request $request)
     {
-        $lst_sv = SinhVien::all();
+        $lst_sv = SinhVien::where('trangthai', '<>', 0)->get();
 
         if (!empty($lst_sv))
             return response()->json(['status' => 'success', 'data' => $lst_sv], 200);
@@ -27,27 +28,46 @@ class SinhVienController extends Controller
     }
     public function store(Request $request)
     {
-     
-        $sv =SinhVien::create([
-            'hosv' => $request->hosv,
-            'tensv' => $request->tensv,
-            'ngaysinh' => $request->ngaysinh,
-            'gioitinh' => $request->gioitinh,
-            'sdt' => $request->sdt,
-            'cccd' => $request->cccd,
-            'malh' => $request->lop,
-            'province_id' => $request->tinh,
-            'district_id' => $request->huyen,
-            'ward_id' => $request->xa,
-            'trangthai' => 1,
 
+        do {
+            $email = rand(0, 9999999999);
+            $len = strlen($email);
+            for ($i = 0; $i < 10 - $len; $i++) {
+                $email = '0' . $email;
+            }
+            $email .= '@caothang.edu.vn';
+            $user = User::where('email', $email)->first();
+        } while (!empty($user));
+
+        $user = User::create([
+            'email' => $email,
+            'trangthai' => 1,
+            'password'  => Hash::make($request->cccd)
         ]);
-        if(!empty($sv)){
-            return response()->json(['status'=>'success','message'=>'Thêm thành công'],200);
+
+        if (!empty($user)) {
+            $sv = SinhVien::create([
+                'hosv' => $request->hosv,
+                'tensv' => $request->tensv,
+                'ngaysinh' => $request->ngaysinh,
+                'gioitinh' => $request->gioitinh,
+                'sdt' => $request->sdt,
+                'cccd' => $request->cccd,
+                'matk' => $user->id,
+                'malh' => $request->lop,
+                'province_id' => $request->tinh,
+                'district_id' => $request->huyen,
+                'ward_id' => $request->xa,
+                'trangthai' => 1,
+
+            ]);
+            if (!empty($sv)) {
+                return response()->json(['status' => 'success', 'message' => 'Thêm thành công'], 200);
+            }
+            return response()->json(['status' => 'error', 'message' => 'Thêm thất bại'], 403);
         }
-        return response()->json(['status'=>'error','message'=>'Thêm thất bại'],403);
     }
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
         $sv = SinhVien::find($id);
         $sv->update([
@@ -64,9 +84,54 @@ class SinhVienController extends Controller
             'trangthai' => 1,
 
         ]);
-        if(!empty($sv)){
-            return response()->json(['status'=>'success','message'=>'Sửa thành công'],200);
+        if (!empty($sv)) {
+            return response()->json(['status' => 'success', 'message' => 'Sửa thành công'], 200);
         }
-        return response()->json(['status'=>'error','message'=>'Sửa thất bại'],403);
+        return response()->json(['status' => 'error', 'message' => 'Sửa thất bại'], 403);
+    }
+    public function delete($id)
+    {
+        $sinhvien = SinhVien::find($id);
+        if (!empty($sinhvien)) {
+            if ($sinhvien->trangthai == 0) {
+                return response()->json(['status' => 'error', 'message' => 'Sinh viên đã bị xóa'], 403);
+            } else {
+                $sinhvien->trangthai = 0;
+                $sinhvien->save();
+                return response()->json(['status' => 'success', 'message' => 'Xóa thành công'], 200);
+            }
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Sinh viên không tồn tại'], 404);
+        }
+    }
+    public function lock($id)
+    {
+        $sinhvien = SinhVien::find($id);
+        if (!empty($sinhvien)) {
+            if ($sinhvien->trangthai == 2) {
+                return response()->json(['status' => 'error', 'message' => 'Sinh viên đã khóa'], 403);
+            } else {
+                $sinhvien->trangthai = 2;
+                $sinhvien->save();
+                return response()->json(['status' => 'success', 'message' => 'Khóa thành công'], 200);
+            }
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Sinh viên không tồn tại'], 404);
+        }
+    }
+    public function unlock($id)
+    {
+        $sinhvien = SinhVien::find($id);
+        if (!empty($sinhvien)) {
+            if ($sinhvien->trangthai == 1) {
+                return response()->json(['status' => 'error', 'message' => 'Sinh viên không bị khóa'], 403);
+            } else {
+                $sinhvien->trangthai = 1;
+                $sinhvien->save();
+                return response()->json(['status' => 'success', 'message' => 'Mở khóa thành công'], 200);
+            }
+        } else {
+            return response()->json(['status' => 'error', 'message' => 'Sinh viên không tồn tại'], 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,44 +17,44 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Route::group(['middleware' => 'auth:api'], function () {
-    Route::prefix('baiviet')->group(function () {
-        Route::get('/discussion-post', 'BaiVietController@getDiscussionPostList');
-        Route::get('/teacher-post', 'BaiVietController@getTeacherPostList');
-        Route::get('detail', 'BaiVietController@detailBoMon');
-        Route::post('create', 'BaiVietController@createNewBoMon');   //chưa xong
-        Route::post('update', 'BaiVietController@updateBoMon');      //chưa xong
-        Route::post('delete', 'BaiVietController@deleteBoMon');
-    });
+Route::prefix('baiviet')->group(function () {
+    Route::get('/discussion-post', 'BaiVietController@getDiscussionPostList');
+    Route::get('/teacher-post', 'BaiVietController@getTeacherPostList');
+    Route::get('detail', 'BaiVietController@detailBoMon');
+    Route::post('create', 'BaiVietController@createNewBoMon');   //chưa xong
+    Route::post('update', 'BaiVietController@updateBoMon');      //chưa xong
+    Route::post('delete', 'BaiVietController@deleteBoMon');
+});
 
-    Route::prefix('khoa')->group(function () {
-        Route::get('/', 'KhoaController@getAllKhoa');
-        Route::get('detail', 'KhoaController@detailKhoa');
-        Route::post('create', 'KhoaController@createNewKhoa');
-        Route::put('update', 'KhoaController@updateKhoa');
-        Route::delete('delete', 'KhoaController@deleteKhoa');
-    });
-    Route::prefix('bomon')->group(function () {
-        Route::get('/', 'BoMonController@getAllBoMon');
-        Route::get('detail', 'BoMonController@detailBoMon');
-        Route::post('create', 'BoMonController@createNewBoMon');
-        Route::put('update', 'BoMonController@updateBoMon');
-        Route::delete('delete', 'BoMonController@deleteBoMon');
-    });
+Route::prefix('khoa')->group(function () {
+    Route::get('/', 'KhoaController@getAllKhoa');
+    Route::get('detail', 'KhoaController@detailKhoa');
+    Route::post('create', 'KhoaController@createNewKhoa');
+    Route::put('update', 'KhoaController@updateKhoa');
+    Route::delete('delete', 'KhoaController@deleteKhoa');
+});
+Route::prefix('bomon')->group(function () {
+    Route::get('/', 'BoMonController@getAllBoMon');
+    Route::get('detail', 'BoMonController@detailBoMon');
+    Route::post('create', 'BoMonController@createNewBoMon');
+    Route::put('update', 'BoMonController@updateBoMon');
+    Route::delete('delete', 'BoMonController@deleteBoMon');
+});
 
-    Route::prefix('sinhvien')->group(function () {
-        Route::get('/', 'SinhVienController@getAllSinhVien');
-        Route::get('/{id}', 'SinhVienController@detail');
-        Route::post('/', 'SinhVienController@store');
-        Route::post('/edit/{id}', 'SinhVienController@update');
-    });
+Route::prefix('sinhvien')->group(function () {
+    Route::get('/', 'SinhVienController@getAllSinhVien');
+    Route::get('/{id}', 'SinhVienController@detail');
+    Route::post('/', 'SinhVienController@store');
+    Route::post('/edit/{id}', 'SinhVienController@update');
+    Route::post('/delete/{id}', 'SinhVienController@delete');
+});
 
-    Route::apiResource('lophocphan', 'LopHocPhanController');
-    Route::apiResource('lophoc', 'LopHocController');
+Route::apiResource('lophocphan', 'LopHocPhanController');
+Route::apiResource('lophoc', 'LopHocController');
 // });
 // Route::get('/home', 'HomeController@index');
 Route::post('/login', 'AuthController@login');
 
-Route::get('/province','ProvinceController@getProvince');
-Route::get('/district','ProvinceController@getDistrict');
-Route::get('/ward','ProvinceController@getWard');
-
+Route::get('/province', 'ProvinceController@getProvince');
+Route::get('/district', 'ProvinceController@getDistrict');
+Route::get('/ward', 'ProvinceController@getWard');
